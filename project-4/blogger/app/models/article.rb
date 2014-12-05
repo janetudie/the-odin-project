e5c6cd094b4966bf7b96cc8ac47aa6b73a835194
Vania Januar <vjanuarcs@gmail.com>
2014-12-05 17:24:05 +1100

    add comment form

--- project-4/blogger/app/models/article.rb
+++ project-4/blogger/app/models/article.rb
@@ -1,4 +1,17 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true,
-		length: { minimum: 5 }
+	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+	validates :title, presence: true
+
+	def tag_list
+  		self.tags.collect do |tag|
+    		tag.name
+  		end.join(", ")
+	end
+
+	def tag_list=(tags_string)
+
+	end
+
 end
