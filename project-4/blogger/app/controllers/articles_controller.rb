b627a53f3dedebbd350984748dd30795a00833ec
Vania Januar <vjanuarcs@gmail.com>
2014-12-05 17:02:07 +1100

    form-based workflow feature completed

--- project-4/blogger/app/controllers/articles_controller.rb
+++ project-4/blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
   		@articles = Article.all
 	end
@@ -8,20 +10,47 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
+		
 		@article = Article.new(article_params)
 
-		if @article.save
-			redirect_to @article
-		else
-			render 'new'
-		end
+		@article.save
+		redirect_to articles_path(@article)
+		# if @article.save
+		# 	redirect_to @article
+		# else
+		#	render 'new'
+		# end
 	end
 
 	def show
 		@article = Article.find(params[:id])
 	end
-	
 
+	def destroy
+  		@article = Article.find(params[:id])
+  		@article.destroy
+
+  		redirect_to articles_path
+  	end
+
+  	def edit
+  		@article = Article.find(params[:id])
+	end
+
+	def update
+  		@article = Article.find(params[:id])
+  		@article.update(article_params)
+  		
+  		flash.notice = "Article '#{@article.title}' Updated!"
+  		
+  		redirect_to article_path(@article)
+	end
+
+	# private
+
+  	# def article_params
+    # 	params.require(:article).permit(:title, :body)
+ 	# end
 
 
 end
